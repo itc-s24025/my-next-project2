13b136dc036724e204215d4df164430c6751c2de
itc-s24025 <s24025@std.it-college.ac.jp>
2025-10-16 12:25:23 +0900

    5-3 終わり

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -1,3 +1,19 @@
+import { createClient } from "microcms-js-sdk";
+import {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSListContent,
+} from "microcms-js-sdk";
+
+// メンバーの型定義
+export type Member = {
+  name: string;
+  position: string;
+  profile: string;
+  image: MicroCMSImage;
+} & MicroCMSListContent;
+
+// ニュースの型定義
 export type News = {
   id: string;
   title: string;
@@ -7,6 +23,29 @@ export type News = {
   publishedAt: string;
   createdAt: string;
 };
+
+// microCMSクライアントの作成
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is not defined");
+}
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is not defined");
+}
+const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+// ニュース一覧を取得
+export const getMembersList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Member>({
+    endpoint: "members",
+    queries: queries,
+  });
+  return listData;
+};
+
+// メンバー詳細を取得
 export type Category = {
   name: string;
 };
