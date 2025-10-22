71a8666d505c43ef154a75e86c3dfe865b71e84e
itc-s24025 <s24025@std.it-college.ac.jp>
2025-10-22 10:43:43 +0900

    ファイルがどこかに行ったので、クローンした。 6章完了

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -61,41 +61,7 @@ export const getNewsDetail = async (
     endpoint: "news",
     contentId,
     queries,
-    customRequestInit: {
-      next: {
-        revalidate: queries?.draftKey === undefined ? 60 : 0,
-      },
-    },
   });
 
   return detailData;
-};
-
-export const getCategoryDetail = async (
-  contentId: string,
-  queries?: MicroCMSQueries
-) => {
-  const detailData = await client.getListDetail<Category>({
-    endpoint: "categories",
-    contentId,
-    queries,
-  });
-
-  return detailData;
-};
-
-export const getAllNewsList = async () => {
-  const listData = await client.getAllContents<News>({
-    endpoint: "news",
-  });
-
-  return listData;
-};
-
-export const getAllCategoryList = async () => {
-  const listData = await client.getAllContents<Category>({
-    endpoint: "categories",
-  });
-
-  return listData;
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import styles from "./page.module.css";
 import Image from "next/image";
 
 import { getNewsList } from "@/app/_libs/microcms";
-import { TOP_NEWS_LIMIT } from "./_contents";
-import NewsList from "@/app/_components/NewsList";
-import ButtonLink from "./_components/ButtoneLink";
+import { TOP_NEWS_LIMIT } from "../app/_contents";
+import NewsList from "../app/_components/NewsList";
+import ButtonLink from "@/app/_components/ButtonLink";
 
 export default async function Home() {
   const data = await getNewsList({
